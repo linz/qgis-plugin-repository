b1daf87145369a084cb7c2e771769550ea48dedc
splanzer <splanzer@linz.govt.nz>
2019-08-20 15:52:52 +1200

    get plugin metadata

--- src/api.py
+++ src/api.py
@@ -1,11 +1,15 @@
 import json
 import logging
 import os
-
+import io
+import re
+import zipfile
 import boto3
+import configparser
 from botocore.exceptions import ClientError
 from flask import Flask, request
 from werkzeug.utils import secure_filename
+
 
 # UPLOAD_ FOLDER = '/tmp/'
 UPLOAD_FOLDER = "/home/splanzer/temp/test"
@@ -19,11 +23,39 @@ if os.environ.get("REPO_BUCKET_NAME") is not None:
 else:
     REPO_BUCKET_NAME = "qgis-plugin-repository"
 
+
 # local testing only
 def format_response(success, http_code, reason="", data=None):
     res_body = {"success": success, "reason": reason, "data": data}
     response = app.response_class(response=json.dumps(res_body), status=http_code, mimetype="application/json")
     return response
+
+
+def metadata_contents(plugin_zip, metadata_path):
+    """
+    Return metadata.txt contents that is stored in
+    the plugin .zip file
+    """
+
+    metadata = plugin_zip.open(metadata_path)
+    metadata = str(metadata.read(), "utf-8")
+    config_parser = configparser.ConfigParser()
+    config_parser.readfp(io.StringIO(metadata))
+    return config_parser
+
+
+def get_metadata_path(plugin_zip):
+    """
+    Returns a list of possible metadata.txt matches
+    The regex applied to the zipfles namelist only matches
+    one dir deep - therefore it will either return an list with
+    one item (path to metadata.txt) else an empty list. If its
+    is an empty list that is returned, the metadata.txt was not found
+    """
+
+    plugin_files = plugin_zip.namelist()
+    metadata_path = [i for i in plugin_files if re.search(r"^\w*\/{1}metadata.txt", i)]
+    return metadata_path
 
 
 def upload_file_to_s3(file_name, bucket, object_name=None):
@@ -51,9 +83,7 @@ def upload_file_to_s3(file_name, bucket, object_name=None):
 
 @app.route("/list", methods=["GET"])
 def list_all():
-    file = request.files["file"]
-    file.save(secure_filename(file.filename))
-    return "file uploaded successfully"
+    return "TODO"
 
 
 @app.route("/upload", methods=["POST"])
@@ -67,24 +97,23 @@ def upload():
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         file.save(filepath)
+        file.close()
 
+        # Extract plugin metadata
+        zfile = zipfile.ZipFile(filepath, "r")
+        metadata_path = get_metadata_path(zfile)
+        metadata_path = metadata_path[0]
+        plugn_metadata = metadata_contents(zfile, metadata_path)
+        pl_name = plugn_metadata["general"]["name"]
+        pl_version = plugn_metadata["general"]["version"]
+        return "{0}-{1}".format(pl_name, pl_version)
         # upload the file
-        success, response = upload_file_to_s3(filepath, REPO_BUCKET_NAME, filename)
-        if success:
-            # logging.info('File was uploaded')
-            return format_response("true", 201)
-
-        return format_response("false", 400, response)
-
-    return format_response("false", 415, "file must be of type .zip")
+        # success, response = upload_file_to_s3(filepath, REPO_BUCKET_NAME, filename)
 
 
 @app.route("/remove", methods=["DELETE"])
 def remove():
-
-    file = request.files["file"]
-    file.save(secure_filename(file.filename))
-    return "file uploaded successfully"
+    return "TODO"
 
 
 if __name__ == "__main__":
