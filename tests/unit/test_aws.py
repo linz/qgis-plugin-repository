040fc2d90a2b6d9abdf864a65631b1e9354e6f20
splanzer <splanzer@linz.govt.nz>
2019-10-10 15:46:30 +1300

    license add as header

--- tests/unit/test_aws.py
+++ tests/unit/test_aws.py
@@ -1,5 +1,14 @@
 """
-Created on 20/09/2019
-
-@author: splanzer
+################################################################################
+#
+# Copyright 2019 Crown copyright (c)
+# Land Information New Zealand and the New Zealand Government.
+# All rights reserved
+#
+# This program is released under the terms of the 3 clause BSD license. See the
+# LICENSE file for more information.
+#
+################################################################################
 """
+
+# There are no tests for this module at this stage
