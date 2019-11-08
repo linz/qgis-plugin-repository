4bd6b8d854856f0e88d870a5a2248e7e17ef4620
splanzer <splanzer@linz.govt.nz>
2019-11-08 15:06:51 +1300

    endpoints for healthchecks

--- src/plugin/api.py
+++ src/plugin/api.py
@@ -210,5 +210,34 @@ def qgis_plugin_xml():
     return app.response_class(response=xml, status=200, mimetype="text/xml")
 
 
+@app.route("/ping", methods=["GET"])
+def ping():
+    """
+    Ping to confirm the service is up
+    """
+
+    return format_response(f"Response in {(time.time() - g.start_time) * 1000}ms", 200)
+
+
+@app.route("/healthcheck", methods=["GET"])
+def healthchek():
+    """
+    Ping to confirm the service is up
+    """
+    checks = {}
+
+    # check database connection
+    MetadataModel.all_version_zeros()
+    checks["database connection"] = "PASS"
+
+    # check s3 connection
+    aws.s3_head_bucket(repo_bucket_name)
+    checks["s3 connection"] = "PASS"
+
+    # Anything not a 200 has been caught as an
+    # error and the healthchecks have failed
+    return format_response(checks, 200)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
