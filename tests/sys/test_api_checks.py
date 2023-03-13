860f731a337bbbb95a0a664543a1e9a706131348
Victor Engmark <vengmark@linz.govt.nz>
2023-03-14 10:14:20 +1300

    test: Show response content for wrong status codes

--- tests/sys/test_api_checks.py
+++ tests/sys/test_api_checks.py
@@ -7,12 +7,12 @@ from .utils import REQUEST_TIMEOUT_SECONDS
 
 def test_ping(config_fixture):
     response = requests.get(f"{config_fixture['base_url']}ping", timeout=REQUEST_TIMEOUT_SECONDS)
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
 
 
 def test_health(config_fixture):
     response = requests.get(f"{config_fixture['base_url']}health", timeout=REQUEST_TIMEOUT_SECONDS)
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     assert json.loads(response.content) == json.loads(response.content)
 
 
