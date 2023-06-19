b85db05829f039623c7917f3fc326a0a9ce5be90
Victor Engmark <vengmark@linz.govt.nz>
2023-06-19 01:48:27 +0000

    test: Return more details (#400)
    * test: Show response content for wrong status codes
    
    * fix: Use default instead of deprecated module
    
    * fix: assert rather than return from test
    
    Should probably be improved with a stronger assertion, but this is the
    smallest step to stop pytest complaining.

--- tests/sys/test_invalid.py
+++ tests/sys/test_invalid.py
@@ -36,7 +36,7 @@ def test_wrong_secrect(config_fixture, stage=""):
     plugin = get_mock_plugin(config_fixture["plugin_id"], config_fixture["plugin_metadata"])
     response = post_plugin(config_fixture["base_url"], stage, config_fixture["plugin_id"], plugin, secert)
 
-    assert response.status_code == 403
+    assert response.status_code == 403, response.content
     assert json.loads(response.content) == {"message": "Invalid token"}
 
 
@@ -45,7 +45,7 @@ def test_is_not_a_zipfile(config_fixture, stage=""):
 
     create_new_record_via_utils(config_fixture)
     response = post_plugin(config_fixture["base_url"], stage, config_fixture["plugin_id"], plugin, config_fixture["secret"])
-    assert response.status_code == 400
+    assert response.status_code == 400, response.content
     assert json.loads(response.content) == {"message": "Plugin file supplied not a Zipfile"}
 
 
@@ -55,7 +55,7 @@ def test_no_data_supplied(config_fixture, stage=""):
 
     create_new_record_via_utils(config_fixture)
     response = post_plugin(config_fixture["base_url"], stage, config_fixture["plugin_id"], plugin, config_fixture["secret"])
-    assert response.status_code == 400
+    assert response.status_code == 400, response.content
     assert json.loads(response.content) == {"message": "No plugin file supplied"}
 
 
@@ -64,21 +64,21 @@ def test_id_is_not_in_db(config_fixture, stage=""):
 
     plugin = get_mock_plugin(config_fixture["plugin_id"], config_fixture["plugin_metadata"])
     response = post_plugin(config_fixture["base_url"], stage, plugin_id, plugin, config_fixture["secret"])
-    assert response.status_code == 400
+    assert response.status_code == 400, response.content
     assert json.loads(response.content) == {"message": "Plugin Not Found"}
 
 
 def test_metadata_file_is_missing(config_fixture, stage=""):
     plugin = get_mock_plugin_no_metadata(config_fixture["plugin_id"])
     response = post_plugin(config_fixture["base_url"], stage, config_fixture["plugin_id"], plugin, config_fixture["secret"])
-    assert response.status_code == 400
+    assert response.status_code == 400, response.content
     assert json.loads(response.content) == {"message": "No metadata.txt file found in plugin directory"}
 
 
 def test_metadata_field_is_missing(config_fixture, stage=""):
     plugin = get_mock_plugin(config_fixture["plugin_id"], config_fixture["plugin_metadata"].replace("name", "nameo"))
     response = post_plugin(config_fixture["base_url"], stage, config_fixture["plugin_id"], plugin, config_fixture["secret"])
-    assert response.status_code == 400
+    assert response.status_code == 400, response.content
     assert json.loads(response.content) == {"message": "Attribute 'name' cannot be None"}
 
 
@@ -86,7 +86,7 @@ def test_invalid_qgis_version(config_fixture):
     qgis_version = "v3"
     response = requests.get(f"{config_fixture['base_url']}plugins.xml?qgis={qgis_version}", timeout=REQUEST_TIMEOUT_SECONDS)
 
-    assert response.status_code == 400
+    assert response.status_code == 400, response.content
     assert json.loads(response.content) == {"message": "Invalid QGIS version"}
 
 
@@ -96,5 +96,5 @@ def test_stage_is_not_valid(config_fixture):
     plugin = get_mock_plugin(config_fixture["plugin_id"], config_fixture["plugin_metadata"].replace("name", "nameo"))
     response = post_plugin(config_fixture["base_url"], stage, config_fixture["plugin_id"], plugin, config_fixture["secret"])
 
-    assert response.status_code == 400
+    assert response.status_code == 400, response.content
     assert json.loads(response.content) == {"message": "stage is not recognised"}
